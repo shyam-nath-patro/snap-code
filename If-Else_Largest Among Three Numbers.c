043d33a6849dfd8ebedf2e97bb76e4b49d6639db
SHYAM NATH PATRO <shyamnathpatro2006@gmail.com>
2025-12-09 20:29:47 +0530

    Create If-Else_Largest Among Three Numbers.c

---
+++ If-Else_Largest Among Three Numbers.c
@@ -0,0 +1,19 @@
+/*Write a program to determine the largest number among three integer numbers.*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    int largest = a;
+    if (b > largest) {
+         largest = b;
+    }
+       
+    if (c > largest){
+        largest = c;
+    } 
+        
+    printf("The largest number is : %d\n", largest);
+    return 0;
+}
