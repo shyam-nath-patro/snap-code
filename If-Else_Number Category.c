1d8fc36d59465fd8c8991e5bf1bd2cc6af1a4a8f
SHYAM NATH PATRO <shyamnathpatro2006@gmail.com>
2025-12-12 16:28:08 +0530

    Update If-Else_Number Category.c

--- If-Else_Number Category.c
+++ If-Else_Number Category.c
@@ -24,7 +24,7 @@ Output Format
 A single character representing the category of N ('A', 'B', 'C', 'D', or 'E').*/
 
 #include <stdio.h>
-
+#include<math.h>
 int main() {
 
     int n;
@@ -33,21 +33,29 @@ int main() {
     {
         printf("A");
     }
-    else if( (n>20) && n==22 || n==33 || n==21|| n==35 || n==55 ||n==77){
+    else if( (n>20) && n==22 || n==33 || n==21|| n==35 || n==55 ||n==77) {
         printf("B");
     }
     else if((n>9 && n<100)&& (n%3==0 || n%7==0)&&(n%21!=0))
     {
         printf("C");
     }
-    else if(n==1||n==4||n==9||n==16||n==25||n==36||n==49||n==64||n==81||n==100||n==121||n==144 || n==196 || n==169)
+    else if(n<200)
     {
-        printf("D");
+        double a = sqrt(n);
+        if (a==(int)a)
+        {
+            printf("D");
+        }
+        else
+        {
+            printf("E");
+        }
     }
     else
-    {
-        printf("E");
-    }
+        {
+            printf("E");
+        }
 
     return 0;
 }
