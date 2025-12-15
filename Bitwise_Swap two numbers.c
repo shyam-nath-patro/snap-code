74e406eac562365a155504488c257dc3e957f0e5
SHYAM NATH PATRO <shyamnathpatro2006@gmail.com>
2025-12-15 21:51:50 +0530

    Create Bitwise_Swap two numbers.c

---
+++ Bitwise_Swap two numbers.c
@@ -0,0 +1,32 @@
+/*Write a program to swap two integers using only the bitwise XOR operator without using a temporary variable.
+
+The program should take two integers, A and B, as input and output the swapped values.
+
+Input Format
+
+The first line contains an integer A
+The second line contains an integer B
+Constraints
+
+(-10^5 ≤ A ≤ 10^5)
+(-10^5 ≤ B ≤ 10^5)
+Output Format
+
+The first line contains the swapped value of A.
+The second line contains the swapped value of B.*/
+
+#include <stdio.h>
+
+int main() {
+    int A, B;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    A = A ^ B;
+    B = A ^ B;
+    A = A ^ B; 
+
+    printf("%d\n%d\n", A, B);
+
+    return 0;
+}
